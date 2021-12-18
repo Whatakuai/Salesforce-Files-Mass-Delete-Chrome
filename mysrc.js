e6941727d951b6e256b0df6acc08984baabe331c
Luisma <luis.rodriguez@adesso.es>
2021-12-18 22:22:27 +0100

    working stable version

--- mysrc.js
+++ mysrc.js
@@ -34,6 +34,12 @@
     100% { transform: rotate(360deg); }
   }`;
   document.head.appendChild (css);
+  let appContextId = '';
+  try {
+    appContextId = window.Aura.initConfig.context.globalValueProviders.filter(elem => elem.type == '$Global')[0].values.appContextId.value
+  }catch(exception){
+    appContextId = ''
+  }
   await Promise.all (
     Array.from (document.querySelectorAll ('a')).map (async elem => {
       if (elem.href.match ('ContentDocument/[a-z|A-Z|0-9]{18}/view')) {
@@ -55,8 +61,8 @@
                 id +
                 '.undefined.null.null.null.Id.VIEW.true.null.ContentUrl%2CExternalDataSourceName%2CParentId%2CLastModifiedDate%2CIsAssetEnabled%2CCreatedBy%3B2LastModifiedById%2CTitle%2CContentSize%2CCreatedBy%3B2CreatedDate%2CCreatedBy%3B2Id%2CRecordType%2CExternalDataSourceType%2CCreatedBy%3B2LastModifiedDate%2CFileType%2CCreatedBy%3B2SystemModstamp%2CFileExtension%2CCreatedBy%3B2Name%2CCreatedDate%2CId%2CLastModifiedById%2CCreatedById.null%22%7D%7D%2C%7B%22id%22%3A%222721%3Ba%22%2C%22descriptor%22%3A%22serviceComponent%3A%2F%2Fui.force.components.controllers.recordGlobalValueProvider.RecordGvpController%2FACTION%24deleteRecord%22%2C%22callingDescriptor%22%3A%22UNKNOWN%22%2C%22params%22%3A%7B%22recordId%22%3A%22' +
                 id +
-                '%22%7D%7D%5D%7D&aura.context=%7B%22mode%22%3A%22PROD%22%2C%22fwuid%22%3A%227FPkrq_-upw5gdD4giTZpg%22%2C%22app%22%3A%22one%3Aone%22%2C%22loaded%22%3A%7B%22APPLICATION%40markup%3A%2F%2Fone%3Aone%22%3A%22B6g-nHwJlr4SfU5_IGg6Ug%22%7D%2C%22dn%22%3A%5B%5D%2C%22globals%22%3A%7B%22density%22%3A%22VIEW_ONE%22%2C%22appContextId%22%3A%2206m090000025AqPAAU%22%7D%2C%22uad%22%3Atrue%7D&aura.pageURI=%2F' +
-                location.pathname +
+                '%22%7D%7D%5D%7D&aura.context=%7B%22mode%22%3A%22PROD%22%2C%22fwuid%22%3A%227FPkrq_-upw5gdD4giTZpg%22%2C%22app%22%3A%22one%3Aone%22%2C%22loaded%22%3A%7B%22APPLICATION%40markup%3A%2F%2Fone%3Aone%22%3A%22B6g-nHwJlr4SfU5_IGg6Ug%22%7D%2C%22dn%22%3A%5B%5D%2C%22globals%22%3A%7B%22density%22%3A%22VIEW_ONE%22%2C%22appContextId%22%3A%22'+appContextId+'%22%7D%2C%22uad%22%3Atrue%7D&aura.pageURI=%2F' +
+                encodeURI(location.pathname) +
                 '&aura.token=' +
                 encodeURI (window.Aura.initConfig.token),
               method: 'POST',
@@ -69,20 +75,5 @@
       }
     })
   );
-  //location.reload ();
-  try{
-    fetch("https://" + location.hostname + "/aura?r=66&ui-force-components-controllers-lists-listViewManagerGrid.ListViewManagerGrid.getRecordLayoutComponent=1&ui-force-components-controllers-relatedList.RelatedListViewDataManager.getItems=1", {
-        headers: {
-            accept: '*/*',
-            'cache-control': 'no-cache',
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-          },
-          referrer: location.href,
-          referrerPolicy: 'origin-when-cross-origin',
-        body: "message=%7B%22actions%22%3A%5B%7B%22id%22%3A%222886%3Ba%22%2C%22descriptor%22%3A%22serviceComponent%3A%2F%2Fui.force.components.controllers.lists.listViewManagerGrid.ListViewManagerGridController%2FACTION%24getRecordLayoutComponent%22%2C%22callingDescriptor%22%3A%22UNKNOWN%22%2C%22params%22%3A%7B%22listReference%22%3A%7B%22listViewIdOrName%22%3A%22CombinedAttachments%22%2C%22inContextOfRecordId%22%3A%2200109000009fx22AAA%22%2C%22isMRU%22%3Afalse%2C%22isSearch%22%3Afalse%7D%2C%22layoutType%22%3A%22RELATED_LIST%22%2C%22layoutMode%22%3A%22VIEW%22%2C%22inContextOfComponent%22%3A%22force%3AlistViewManagerGrid%22%2C%22enableMassActions%22%3Atrue%2C%22enableRowErrors%22%3Atrue%2C%22useHoversForLookup%22%3Afalse%7D%7D%2C%7B%22id%22%3A%222889%3Ba%22%2C%22descriptor%22%3A%22serviceComponent%3A%2F%2Fui.force.components.controllers.relatedList.RelatedListViewDataManagerController%2FACTION%24getItems%22%2C%22callingDescriptor%22%3A%22UNKNOWN%22%2C%22params%22%3A%7B%22filterName%22%3A%22CombinedAttachments%22%2C%22pageSize%22%3A50%2C%22sortBy%22%3A%22%22%2C%22limit%22%3A1000%2C%22useTimeout%22%3Afalse%2C%22queryLocator%22%3Anull%2C%22parentRecordId%22%3A%2200109000009fx22AAA%22%2C%22getCount%22%3Afalse%2C%22isPreview%22%3Afalse%2C%22enableRowActions%22%3Afalse%2C%22offset%22%3A0%7D%2C%22storable%22%3Atrue%7D%5D%7D&aura.context=%7B%22mode%22%3A%22PROD%22%2C%22fwuid%22%3A%227FPkrq_-upw5gdD4giTZpg%22%2C%22app%22%3A%22one%3Aone%22%2C%22loaded%22%3A%7B%22APPLICATION%40markup%3A%2F%2Fone%3Aone%22%3A%22CH-V0kZ54c2UDU8-tYTXdA%22%7D%2C%22dn%22%3A%5B%5D%2C%22globals%22%3A%7B%22density%22%3A%22VIEW_ONE%22%2C%22appContextId%22%3A%2206m090000025AqPAAU%22%7D%2C%22uad%22%3Atrue%7D&aura.pageURI=%2Flightning%2Fr%2FCombinedAttachment%2F00109000009fx22AAA%2Frelated%2FCombinedAttachments%2Fview&aura.token=eyJub25jZSI6Inp2UFdRcHctTlVjZWJqRnQ1dHREZzZnNFozVkJXcUhtN0FJTUU3d0xIM0FcdTAwM2QiLCJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6IntcInRcIjpcIjAwRDA5MDAwMDAwR3lYWVwiLFwidlwiOlwiMDJHMDkwMDAwMDAwWXFYXCIsXCJhXCI6XCJjYWltYW5zaWduZXJcIn0iLCJjcml0IjpbImlhdCJdLCJpYXQiOjE2Mzk3NTA5MDk0MTcsImV4cCI6MH0%3D..bLmwqOUfBqVrVmX68WVMaXD-yEV1QQzT0gAAXPyvvGQ%3D",
-        method: "POST"
-      });
-  } catch (exception) {
-    location.reload()
-  }
+  location.reload ();
 }) ();
