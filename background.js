20f078fb1d19290c84b4ab924f1fd2600cc9e02d
Luisma <luis.rodriguez@adesso.es>
2021-12-25 22:26:04 +0100

    add buy me a coffee

--- background.js
+++ background.js
@@ -1,12 +1,15 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo && changeInfo.status === 'complete') {
-    chrome.tabs.sendMessage(tabId, {
-        message: 'TabUpdated',
-        url: tab.url,
-        changeInfo: changeInfo
+chrome.tabs.onUpdated.addListener (function (tabId, changeInfo, tab) {
+  if (changeInfo && changeInfo.status === 'complete') {
+    chrome.tabs.sendMessage (tabId, {
+      message: 'TabUpdated',
+      url: tab.url,
+      changeInfo: changeInfo,
     });
-    }
-})
-  
+  }
+});
 
-
+chrome.runtime.onInstalled.addListener (function (details) {
+  if (details.reason === 'install') {
+    chrome.tabs.create ({url: 'https://www.buymeacoffee.com/whatakuai'});
+  }
+});
